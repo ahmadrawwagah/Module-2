58e77f57c2358a521419271e1701b5d631b86771
ahmadrawwagah <96959925+ahmadrawwagah@users.noreply.github.com>
2024-04-03 22:56:14 -0400

    Update reader.py

--- reader.py
+++ reader.py
@@ -8,7 +8,7 @@ import serial
 ser = serial.Serial('/dev/ttyS8', 115200)
 scope = 'user-modify-playback-state'
 username = "ahmadrawwagah"
-token = util.prompt_for_user_token(username,scope,client_id='d5a7d3ad4ce9477e90cb48e781b9dd29',client_secret='adceb38738a84fb2bacdb2a7a17e5353',redirect_uri='http://localhost/:8080')
+token = util.prompt_for_user_token(username,scope,client_id='<INSERT_CLIENT_ID>',client_secret='<INSERT_CLIENT_SECRET>',redirect_uri='http://localhost/:8080')
 if token:
     print("Got Token")
 else:
